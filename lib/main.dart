bac4bcbbf5c127f4f922749d684251f9e0cad142
Your Name <lamializa991@gmail.com>
2025-03-18 11:25:34 +0600

    add image card

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
-import 'package:flutter/material.dart';
-import 'screens/home/page/home.dart';
-
-void main() {
-  runApp(const FreebeesEcom());
-}
-
-class FreebeesEcom extends StatelessWidget {
-  const FreebeesEcom({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Freebees E-com",
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
-  }
-}
+import 'package:e_commerce_app/screens/home/page/home_screen.dart';
+ import 'package:e_commerce_app/screens/home/widgets/bottom_nav_bar.dart';
+ import 'package:flutter/material.dart';
+ import 'package:icons_plus/icons_plus.dart';
+ @@ -14,31 +15,32 @@
+ class _HomeState extends State<Home> {
+   int _currentIndex = 0;
+   final List<Widget> _screens = [
+     Container(height: double.infinity, color: Colors.amberAccent),
+     //Container(height: double.infinity, color: Colors.amberAccent),
+     HomeScreen(),
+     Container(height: double.infinity, color: Colors.green),
+     Container(height: double.infinity, color: Colors.blue),
+     Container(height: double.infinity, color: Colors.red),
+   ];
+ 
+   void _onTapMethod(int index) {
+     setState(() {
+       _currentIndex = index;
+     });
+   }
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: FAppBar(
+         title: 'Mega Shop',
+         firstIcon: Iconsax.notification_bing_outline,
+         //backButton: true,
+       ),
+       body: _screens[_currentIndex],
+       bottomNavigationBar: BottomBar(
+         currentIndex: _currentIndex,
+         onTapMethod: _onTapMethod,
+       ),
+     );
+   }
+ }
